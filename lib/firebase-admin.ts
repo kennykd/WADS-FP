2c4ba4ef4e9ed22a7af8e5665f989809ec47e7de
Barrizzz <barripratama.bp@gmail.com>
2026-02-12 17:00:38 +0700

    chore: Update Firebase Admin.

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -4,11 +4,11 @@ import admin from "firebase-admin"
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert({
-            projectId:"",
-            clientEmail:"",
-            privateKey:""
-        })
-    })
-}
+            projectId:process.env.PROJECT_ID!,
+            clientEmail:process.env.CLIENT_EMAIL!,
+            privateKey:process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        }),
+    });
+};
 
 export const adminAuth = admin.auth()
